edf42c2c2b94050d3f2a38a6bfc21966b43998f8
PIU182 <150256780+PIU182@users.noreply.github.com>
2026-05-24 13:06:25 +0700

    Add files via upload

---
+++ [ Luyện tập ] Quản lý thư viện sách.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE LibraryDB;
+
+CREATE SCHEMA library;
+
+CREATE TABLE library.Books (
+    book_id SERIAL PRIMARY KEY,
+    title VARCHAR(100),
+    author VARCHAR(100),
+    published_year INT,
+    available BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE library.Members (
+    member_id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    join_date DATE DEFAULT CURRENT_DATE
+);
